27ca4f86f8676e07a6ec9702328970482ac47a26
SahalShaParveen <sahalshaparveen@gmail.com>
2026-06-02 12:56:35 +0000

    Add motor testing

--- src/wobble.ino
+++ src/wobble.ino
@@ -16,12 +16,12 @@ const int LOOP_DELAY_MS = 20;
 const float DT  = LOOP_DELAY_MS /1000.0; 
 
 float angle = 0; 
-float gyBias = -0.60; 
+float gyBias = -0.62; 
 
 const float BALANCE_ANGLE = 3.3; 
 
 void readAccel(int16_t &ax, int16_t &ay, int16_t &az) {
-        Wire.beginTransmission(MPU); 
+    Wire.beginTransmission(MPU); 
     Wire.write(0x3B); 
     Wire.endTransmission(false); 
 
@@ -44,6 +44,38 @@ void readGyro(int16_t &gx, int16_t &gy, int16_t &gz) {
     gy = Wire.read() << 8 | Wire.read();
     gz = Wire.read() << 8 | Wire.read();
 }
+
+
+void setMotorSpeed(int speed) {
+    speed = constrain(speed, -255, 255);
+
+    // analogWrite(ENA, abs(speed)); 
+    // analogWrite(ENB, abs(speed)); 
+    analogWrite(ENA, 255); analogWrite(ENB, 255);  
+
+    if (speed > 0) {
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, LOW);
+    }
+    else if (speed < 0) {
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, HIGH);
+    }
+    else {
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
+    }
+}
+
 
 void setup(){
     pinMode(ENA, OUTPUT); 
@@ -81,4 +113,12 @@ void loop() {
     float error = angle - BALANCE_ANGLE; 
     Serial.println(error); 
     delay(LOOP_DELAY_MS); 
+
+    if (error > 10) {
+        setMotorSpeed(100); 
+    } else if (error < -10) {
+        setMotorSpeed(-100); 
+    } else {
+        setMotorSpeed(0); 
+    }
 }
